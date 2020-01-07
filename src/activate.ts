33dc160be0f678624e78642dc36966d55b746260
zhanzizhen <zhanzizhen@corp.neteaze.com>
2020-01-07 12:55:42 +0800

    improvement: 优化代码架构，完善配置文件的默认配置

--- src/activate.ts
+++ src/activate.ts
@@ -1,98 +1,98 @@
 import * as vscode from "vscode";
 import defaultConfigStr from "./defaultConfig";
 
-export default function activate(context: vscode.ExtensionContext) {
-  const { workspace, Uri, window } = vscode;
-  // user's input name
-  let componentName = "";
-  let config = {};
-  // the first file we create
-  let firstFileUri: vscode.Uri;
+const { workspace, Uri, window } = vscode;
+// user's input name
+let componentName = "";
+// easy-component.config.js
+let config = {};
+// the first file we create
+let firstFileUri: vscode.Uri;
 
-  function transStrToUnit8(value: string): Uint8Array {
-    return new Uint8Array(Buffer.from(value));
+function transStrToUnit8(value: string): Uint8Array {
+  return new Uint8Array(Buffer.from(value));
+}
+
+function resolvePath(fileName: string): vscode.Uri {
+  return Uri.parse("file://" + "/" + workspace.rootPath + "/" + fileName);
+}
+
+// parse the config object to files
+async function parseObject(obj: object, prePath: string) {
+  const entries = Object.entries(obj);
+  for (const [key, value] of entries) {
+    const pathName = prePath + "/" + key;
+    if (typeof value === "object") {
+      await parseObject(value, pathName);
+    } else {
+      const fileUri = resolvePath(replaceTemplate(pathName));
+      const data = transStrToUnit8(replaceTemplate(value));
+      if (!firstFileUri) {
+        firstFileUri = fileUri;
+      }
+      await workspace.fs.writeFile(fileUri, data);
+    }
   }
+}
 
-  function resolvePath(fileName: string): vscode.Uri {
-    return Uri.parse("file://" + "/" + workspace.rootPath + "/" + fileName);
-  }
+function replaceTemplate(value: string): string {
+  return value.replace(/{{Name}}/g, componentName);
+}
 
-  async function handleAfterRegister() {
-    // parse the config object to files
-    async function parseObject(obj: object, prePath: string) {
-      const entries = Object.entries(obj);
-      for (const [key, value] of entries) {
-        const pathName = prePath + "/" + key;
-        if (typeof value === "object") {
-          await parseObject(value, pathName);
-        } else {
-          const fileUri = resolvePath(replaceTemplate(pathName));
-          const data = transStrToUnit8(replaceTemplate(value));
-          if (!firstFileUri) {
-            firstFileUri = fileUri;
-          }
-          await workspace.fs.writeFile(fileUri, data);
-        }
-      }
-    }
-
-    function replaceTemplate(value: string): string {
-      return value.replace(/{{Name}}/g, componentName);
-    }
-
-    async function handdleError(e: any) {
-      // easy-component.config is not found
-      if (e.code === "MODULE_NOT_FOUND") {
-        const fileUri = resolvePath("easy-component.config.js");
-        await workspace.fs.writeFile(
-          fileUri,
-          transStrToUnit8(defaultConfigStr)
-        );
-        await window.showTextDocument(fileUri, {
-          preserveFocus: true
-        });
-        window.showErrorMessage("请先配置easy-component.config.js");
-      } else {
-        window.showErrorMessage(e);
-      }
-    }
-
-    // create components files now
-    async function createFiles() {
-      await parseObject(config, "");
-      if (firstFileUri) {
-        window.showTextDocument(firstFileUri, {
-          preserveFocus: true
-        });
-        window.showInformationMessage("已在根目录生成组件模板!");
-      }
-    }
-
-    try {
-      const path = workspace.rootPath + "/" + "easy-component.config.js";
-      // use commonjs to load config
-      config = require(path);
-    } catch (e) {
-      handdleError(e);
-      return;
-    }
-
-    const fileName = await window.showInputBox({
-      placeHolder: "会替换配置文件的{{Name}}",
-      prompt: "输入组件的名字"
+async function onMissingConfigFile(e: any) {
+  // easy-component.config is not found
+  if (e.code === "MODULE_NOT_FOUND") {
+    const fileUri = resolvePath("easy-component.config.js");
+    await workspace.fs.writeFile(fileUri, transStrToUnit8(defaultConfigStr));
+    await window.showTextDocument(fileUri, {
+      preserveFocus: true
     });
+    window.showErrorMessage("请先配置easy-component.config.js");
+  } else {
+    window.showErrorMessage(e.message);
+  }
+}
 
-    // if the user's input is not empty or canceled
-    if (fileName) {
-      componentName = fileName;
-      createFiles();
-    }
+// create components files now
+async function createFiles() {
+  await parseObject(config, "");
+  if (firstFileUri) {
+    // after creating files, make the vscode window focus on the frist created file
+    window.showTextDocument(firstFileUri, {
+      preserveFocus: true
+    });
+    window.showInformationMessage("已在根目录生成组件模板!");
+  }
+}
+
+async function handleAfterRegisterCommand() {
+  try {
+    const configPath = workspace.rootPath + "/" + "easy-component.config.js";
+    // use commonjs to load config
+    config = require(configPath);
+  } catch (e) {
+    onMissingConfigFile(e);
+    return;
   }
 
+  // show input
+  const fileName = await window.showInputBox({
+    placeHolder: "会替换配置文件的{{Name}}",
+    prompt: "输入组件的名字"
+  });
+
+  // if the user's input is not empty or canceled
+  if (fileName) {
+    componentName = fileName;
+    createFiles();
+  }
+}
+
+export default function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     // The command has been defined in the package.json file
     "extension.createComponent",
-    handleAfterRegister
+    handleAfterRegisterCommand
   );
 
   context.subscriptions.push(disposable);
--- src/defaultConfig.ts
+++ src/defaultConfig.ts
@@ -1,26 +1,17 @@
 export default `
-// {{Name}}：你在vscode输入框的输入值
-/*
-举例1：
-"{{Name}}": {
-    "{{Name}}.jsx": jsContent,
-    "{{Name}}.scss": cssContent,
-  }
-  当你在输入框输入App时，最后的产生结果是：
-  -App
-    -App.jsx
-    -App.scss
-*/
+// easy-component, a vscode extension, works for crease coding efficience.
 
-const jsContent = \`import React from 'react'
+const jsContent = \`import React from 'react';
 
-class {{Name}} extends React.component{
-  render(){
+class {{Name}} extends React.Component {
+  render() {
     return (
       <div>hello world</div>
     )
   }
 }
+
+export default {{Name}};
 \`;
 
 const cssContent = \`.root{
@@ -28,11 +19,31 @@ const cssContent = \`.root{
 }
 \`;
 
+const indexContent = \`import {{Name}} from './{{Name}}';
+
+export default {{Name}};
+\`;
+
 module.exports = {
   // {{Name}}是你在vscode输入框的输入值
   '{{Name}}': {
     '{{Name}}.jsx': jsContent, // 配置你的js文件内容
     '{{Name}}.scss': cssContent, // 配置你的css文件内容
+    'index.js': indexContent, // Component entry
   },
 };
 `;
+
+/*
+{{Name}}：你在vscode输入框的输入值
+举例： 配置"{{Name}}": {
+            "{{Name}}.jsx": jsContent,
+            "{{Name}}.scss": cssContent,
+          }
+  当你在输入框输入App时，产生结果是下面这些文件：
+  -App
+    -App.jsx
+    -App.scss
+  
+  github: https://github.com/zhanzizhen/easy-component
+*/
